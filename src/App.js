44c28e8eba08f57cd583ce6583d66a8d266f4062
shahyash1136 <43175218+shahyash1136@users.noreply.github.com>
2020-06-13 18:24:22 +0530

    state in class components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import Greet from './components/Greet';
-import Welcome from './components/Welome'
-
+import Welcome from './components/Welome';
+import Message from './components/Message';
 function App() {
     return (
         <div className="App">
-            <Greet name='Bruce' heroName='Batman'/>
+
+            <Message/>
+
+            {/* <Greet name='Bruce' heroName='Batman'/>
             <Greet name='Clark' heroName='Superman'/>
             <Greet name='Diana' heroName='Wonder Woman'/>
             <Welcome name='Bruce' heroName='Batman'/>
             <Welcome name='Clark' heroName='Superman'/>
-            <Welcome name='Diana' heroName='Wonder Woman'/>
+            <Welcome name='Diana' heroName='Wonder Woman'/> */}
         </div>
     );
 }
